f2a9bf188a7d682ce4731f2da6e3fe04a6d710c6
Fiolka1 <matihati60@gmail.com>
2024-02-15 19:12:32 +0100

    setup the candles group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,10 @@ import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgIf],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
-  visibility=false;
-
-  
-
-  
+  visibility = false;
 }
