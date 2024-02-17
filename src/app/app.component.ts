f8adbc656ba3babc00b403461758e6221ec87abf
Fiolka1 <matihati60@gmail.com>
2024-02-17 13:51:20 +0100

    making a image componet for candles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { ImagesComponent } from './images/images.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, NgIf,NgFor,ImagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   visibility = false;
+
 }
