5d9be081b6dc20db0af43b23aed30d952da72b53
Fiolka1 <matihati60@gmail.com>
2024-02-17 20:56:03 +0100

    added angular-cli-ghpages

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-images',
@@ -69,7 +69,7 @@ export class ImagesComponent {
   ngOnInit() {
     for (let index = 1; index < 5; index++) {
       let candle_object = {
-        source: 'assets/images/candles/CitrusZephyr-'+[index]+'.jpg',
+        source: 'assets/images/candles/CitrusZephyr-' + [index] + '.jpg',
         alternate: 'Citrus Zephyr',
         description:
           ' A refreshing fusion of zesty lemon, tangy grapefruit, and a breeze of sweet orange blossom.',
@@ -78,7 +78,7 @@ export class ImagesComponent {
     }
     for (let index = 1; index < 5; index++) {
       let candle_object = {
-        source: 'assets/images/candles/CozyCampfire-'+[index]+'.jpg',
+        source: 'assets/images/candles/CozyCampfire-' + [index] + '.jpg',
         alternate: 'Cozy Campfire',
         description:
           'The perfect combination of smoky cedarwood, toasted marshmallows, and a touch of cinnamon spice.',
@@ -87,7 +87,7 @@ export class ImagesComponent {
     }
     for (let index = 1; index < 5; index++) {
       let candle_object = {
-        source: 'assets/images/candles/GoldenSunset-'+[index]+'.jpg',
+        source: 'assets/images/candles/GoldenSunset-' + [index] + '.jpg',
         alternate: 'Golden Sunset',
         description:
           ' A warm and inviting mix of amber, vanilla bean, and a hint of sandalwood.',
@@ -96,7 +96,7 @@ export class ImagesComponent {
     }
     for (let index = 1; index < 5; index++) {
       let candle_object = {
-        source: 'assets/images/candles/LibraryNostalgia-'+[index]+'.jpg',
+        source: 'assets/images/candles/LibraryNostalgia-' + [index] + '.jpg',
         alternate: 'Library Nostalgia',
         description:
           'It could capture the scent of old books, with notes of aged paper, weathered leather, and a hint of vanilla.',
@@ -105,7 +105,7 @@ export class ImagesComponent {
     }
     for (let index = 1; index < 5; index++) {
       let candle_object = {
-        source: 'assets/images/candles/MoonlitGarden-'+[index]+'.jpg',
+        source: 'assets/images/candles/MoonlitGarden-' + [index] + '.jpg',
         alternate: 'Moonlit Garden',
         description:
           ' A romantic combination of night-blooming jasmine, delicate lily of the valley, and a hint of moonflower.',
@@ -114,7 +114,7 @@ export class ImagesComponent {
     }
     for (let index = 1; index < 5; index++) {
       let candle_object = {
-        source: 'assets/images/candles/MysticMoonstone-'+[index]+'.jpg',
+        source: 'assets/images/candles/MysticMoonstone-' + [index] + '.jpg',
         alternate: 'Mystic Moonstone',
         description:
           'A captivating blend of lavender, bergamot, and a sprinkle of moonlit jasmine.',
@@ -123,7 +123,7 @@ export class ImagesComponent {
     }
     for (let index = 1; index < 5; index++) {
       let candle_object = {
-        source: 'assets/images/candles/WhimsicalDreams-'+[index]+'.jpg',
+        source: 'assets/images/candles/WhimsicalDreams-' + [index] + '.jpg',
         alternate: 'Whimsical Dreams',
         description:
           'A magical fusion of cotton candy, sparkling lemonade, and a touch of rainbow sherbet.',
@@ -132,21 +132,25 @@ export class ImagesComponent {
     }
     for (let index = 1; index < 5; index++) {
       let candle_object = {
-        source: 'assets/images/candles/WhisperingWoods-'+[index]+'.jpg',
+        source: 'assets/images/candles/WhisperingWoods-' + [index] + '.jpg',
         alternate: 'Whispering Woods',
         description:
           ' A blend of fresh pine, crisp eucalyptus, and a touch of misty rain.',
       };
       this.candles.push(candle_object);
     }
-
-
-
-
     for (let index = 0; index < 8; index++) {
       let random_candle = Math.floor(Math.random() * 40);
       this.candles_menu.unshift(this.candles[random_candle]);
     }
     this.candles_menu.pop();
+    
+
+  }
+  
+  candleFull(ewent: any) {
+    let candleNames = ewent;
+    console.log(candleNames);
+    
   }
 }
